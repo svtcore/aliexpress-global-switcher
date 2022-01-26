8774a98473b13b97022d9699fefb7b3fb066d710
svtcore <67793906+svtcore@users.noreply.github.com>
2022-01-27 01:26:10 +0200

    added global search switcher

--- js/content_script.js
+++ js/content_script.js
@@ -1,7 +1,9 @@
 /**
- * @author github.com/svtcore
+ * @author svtcore
+ * @license GPL-3.0
+ * @link https://github.com/svtcore
+ * 
  */
-
 
 chrome.storage.sync.get("alidata", function(obj) {
     if (typeof obj.alidata != 'undefined') {
@@ -13,14 +15,38 @@ chrome.storage.sync.get("alidata", function(obj) {
         var region = getCurrentRegion(cookie_arr).toUpperCase();
         var b_locale = getCurrentLocale(cookie_arr);
         var ajax = false;
-        var domain = "com";
-        if (alidata[0].country_currency_mode) {
-            if (site == "rus") domain = "ru";
-            if ((alidata[0].currency).toUpperCase() != currency || (alidata[0].region).toUpperCase() != region) {
-                cookielink = "https://login.aliexpress." + domain + "/setCommonCookie.htm?fromApp=false&currency=" + alidata[0].currency + "&region=" + (alidata[0].region).toUpperCase() + "&bLocale=" + b_locale + "&site=" + site + "&province=&city=";
+        var global_url = "";
+        if (current_url.search("aliexpress.ru") != -1) {
+            //if page aliexpress.ru
+            //check if enabled redirect to global version
+            if (alidata[0].global_mode) {
+                //if enabled redirect to global
+                chrome.storage.sync.remove('alidata_temp', function() {
+                    console.log('Temp link has been deleted from storage');
+                });
+                global_url = (current_url.replace('aliexpress.ru', 'aliexpress.com')).split("?")[0];
+                createTempLink(global_url);
+                cookielink = "https://login.aliexpress.com/setCommonCookie.htm?fromApp=false&currency=" + currency + "&region=" + region + "&bLocale=" + alidata[0].locale + "&site=" + alidata[0].site + "&province=&city=";
                 ajax = true;
+            } else {
+                //if not enabled global redirect then check if turned on country and currency mode
+                if (alidata[0].country_currency_mode) {
+                    if ((alidata[0].currency).toUpperCase() != currency || (alidata[0].region).toUpperCase() != region) {
+                        cookielink = "https://login.aliexpress.ru/setCommonCookie.htm?fromApp=false&currency=" + alidata[0].currency + "&region=" + (alidata[0].region).toUpperCase() + "&bLocale=" + b_locale + "&site=" + site + "&province=&city=";
+                        ajax = true;
+                    }
+                }
+            }
+        } else {
+            //if opened global version check if turned on country and currency mode
+            if (alidata[0].country_currency_mode) {
+                if ((alidata[0].currency).toUpperCase() != currency || (alidata[0].region).toUpperCase() != region) {
+                    cookielink = "https://login.aliexpress.com/setCommonCookie.htm?fromApp=false&currency=" + alidata[0].currency + "&region=" + (alidata[0].region).toUpperCase() + "&bLocale=" + b_locale + "&site=" + site + "&province=&city=";
+                    ajax = true;
+                }
             }
         }
+        //ajax true if need to change settings
         if (ajax) {
             $.ajax({
                 type: "GET",
@@ -29,6 +55,18 @@ chrome.storage.sync.get("alidata", function(obj) {
                     withCredentials: true,
                 },
                 success: function(msg) {
+                    if (alidata[0].global_mode) {
+                        console.log(msg);
+                        chrome.storage.sync.get('alidata_temp', function(obj) {
+                            if (typeof obj.alidata_temp != 'undefined') {
+                                var data = JSON.parse(obj.alidata_temp);
+                                link = String(data[data.length - 1].link);
+                                window.location.href = link;
+                            } else {
+                                window.location.href = current_url;
+                            }
+                        });
+                    }
                     if (alidata[0].country_currency_mode) {
                         window.location.href = current_url;
                     }
@@ -77,16 +115,27 @@ function getCurrentLocale(cookie_arr) {
 }
 
 function getCookies() {
-    var cookies = document.cookie;
-    var cookies_arr = cookies.split(';');
-    for (var i = 0; i < cookies_arr.length; i++) {
-        var val = cookies_arr[i].trim();
+    var x = document.cookie;
+    var y = x.split(';');
+    for (var i = 0; i < y.length; i++) {
+        var val = y[i].trim();
         if (val.startsWith('aep_usuc_f')) {
             var aep = val.split('&');
             aep[0] = aep[0].replace("aep_usuc_f=", "");
             return aep;
         }
     }
+}
+
+function createTempLink(value) {
+    var arrayArg = new Array();
+    var jsonArg = new Object();
+    jsonArg.link = value;
+    arrayArg.push(jsonArg);
+    var jsonArray = JSON.stringify(arrayArg);
+    chrome.storage.sync.set({ "alidata_temp": jsonArray }, function() {
+        console.log("Created key with temp link")
+    });
 }
 
 function createDefaultKey() {
@@ -96,6 +145,7 @@ function createDefaultKey() {
     jsonArg.region = "US";
     jsonArg.locale = "en_US";
     jsonArg.site = "glo";
+    jsonArg.global_mode = 0;
     jsonArg.country_currency_mode = 0;
     arrayArg.push(jsonArg);
     var jsonArray = JSON.stringify(arrayArg);
